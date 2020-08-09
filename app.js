a7825159b43f8100a9a1bfc415f15a17dcb553f5
zeyneppinar <zeynepinarkaya@gmail.com>
2020-08-09 23:11:18 +0300

    fixed semantics

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.querySelector('.banner-btn').addEventListener('click',()=>{
     document.querySelector('.banner').style.display='none';
-    document.querySelector('.form-container').style.cssText='opacity 1; visibility:visible';
+    document.querySelector('.form-container').style.cssText='opacity: 1; visibility:visible';
     document.querySelector('.container').style.background ='#cc683c';
 })
 
